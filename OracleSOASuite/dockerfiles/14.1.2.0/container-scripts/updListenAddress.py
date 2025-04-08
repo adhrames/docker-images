aa87e481c8741dbc09eb6c19299caf9b1223b8f1
adhithya.ramesh@oracle.com <adhrames@k8sdev-ol9-asharao.subnet1regional.paasinfratoophx.oraclevcn.com>
2025-04-08 16:37:45 +0000

    Folder changed to 14.1.2.0

--- OracleSOASuite/dockerfiles/14.1.2.0/container-scripts/updListenAddress.py
+++ OracleSOASuite/dockerfiles/14.1.2.0/container-scripts/updListenAddress.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2019, 2021, Oracle and/or its affiliates.
+# Copyright (c) 2019, 2025, Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
